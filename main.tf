0c1b149513633fbe045508de44918d970b92cfe3
adityavaste <adityavaste@gmail.com>
2025-12-26 15:55:25 +0530

    Change availability zone for private subnet

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id            = aws_vpc.eks_vpc.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = "ap-south-1b"
 
   tags = { Name = "private-subnet" }
 }
