f88be16e78c4434073da2323059625e89c7db04c
adityavaste <adityavaste@gmail.com>
2025-12-27 14:52:56 +0530

    Set max_size to 1 in scaling_config

--- main.tf
+++ main.tf
@@ -165,7 +165,7 @@ resource "aws_eks_node_group" "nodes" {
   scaling_config {
     desired_size = 1
     min_size     = 1
-    max_size     = 3
+    max_size     = 1
   }
 
   instance_types = ["t2.micro"]
